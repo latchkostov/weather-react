b2df28e53e29d7986258e2144754c1854f618090
latchkostov <latch.kostov@gmail.com>
2022-09-25 18:28:47 -0500

    one more place

--- utils/temperature-utils.ts
+++ utils/temperature-utils.ts
@@ -10,11 +10,11 @@ export const getDayPrefenceDisplayString = (
 ) => {
   return {
     minTemp:
-      temperatureUnit === 0
+      temperatureUnit === TempUnit.Celcius
         ? tempObject.mintemp_c + degreesC
         : tempObject.mintemp_f + degreesF,
     maxTemp:
-      temperatureUnit === 0
+      temperatureUnit === TempUnit.Fahrenheit
         ? tempObject.maxtemp_c + degreesC
         : tempObject.maxtemp_f + degreesF,
   };
