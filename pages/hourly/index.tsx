91b2b506b6f68189db101d88e9e047c0fc6ff3dd
Latch Kostov <latch.kostov@gmail.com>
2022-10-24 19:56:36 -0500

    fix build error

--- pages/hourly/index.tsx
+++ pages/hourly/index.tsx
@@ -1,5 +1,4 @@
 import { useSelector } from "react-redux";
-import Router from "next/router";
 import DayHourly from "../../components/day-hourly";
 import { RootState } from "../../store/store";
 
@@ -7,7 +6,6 @@ const Hourly = () => {
   const forecastDay = useSelector((state: RootState) => state.forecastDay);
   if (!forecastDay?.day?.date) {
     console.log("no forecast day");
-    Router.push("/");
   }
   console.log(forecastDay);
   return <DayHourly hourly={{}}></DayHourly>;
