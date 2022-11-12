48263f25dbb23d6adb330f65991ac3b5758d2f27
Latch Kostov <latch.kostov@gmail.com>
2022-11-12 17:42:28 -0600

    use @next/fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,23 @@
 import { store } from "../store/store";
 import { Provider } from "react-redux";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Roboto } from "@next/font/google";
 
 import "../styles/globals.scss";
 
 const queryClient = new QueryClient();
 
+const roboto = Roboto({
+  weight: "400",
+});
+
 function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
+        <main className={roboto.className}>
+          <Component {...pageProps} />
+        </main>
       </QueryClientProvider>
     </Provider>
   );
