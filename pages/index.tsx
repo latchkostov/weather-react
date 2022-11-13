bc8106c588ff1e429442709d6adf2841e5eeaa29
Latch Kostov <latch.kostov@gmail.com>
2022-11-13 13:23:16 -0600

    commit from gitkraken desktop

--- pages/index.tsx
+++ pages/index.tsx
@@ -68,7 +68,7 @@ export default function Home() {
 
   if (forecastWeatherError)
     return "An error has occurred: " + forecastWeatherError;
-  console.log('test');
+  // console.log('test');
 
   return (
     <div className={`w-full h-screen flex flex-col`}>
