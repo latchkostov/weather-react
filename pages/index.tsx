ec73c4ce2908140b934485bb8a4d24fabbc6fb0e
Latch Kostov <latch.kostov@gmail.com>
2022-11-13 14:54:13 -0600

    cmd

--- pages/index.tsx
+++ pages/index.tsx
@@ -68,7 +68,6 @@ export default function Home() {
 
   if (forecastWeatherError)
     return "An error has occurred: " + forecastWeatherError;
-  console.log('test');
 
   return (
     <div className={`w-full h-screen flex flex-col`}>
